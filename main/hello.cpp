9f50d7768c96face0f58c2521b750e52cb7aec77
Smaran Jawalkar <smaran.jawalkar@gmail.com>
2024-03-05 15:13:24 +0530

    Added my name to cpp file

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,5 +6,7 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run\n";
+  cout << "My name is Smaran!\n";
+  cout << "My SRN is PES2UG21CS459 :)\n";
   return 0;
 }
