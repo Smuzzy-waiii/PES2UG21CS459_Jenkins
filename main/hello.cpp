a3a760c5487301b3865063c935c0f912d4506b67
Smaran Jawalkar <smaran.jawalkar@gmail.com>
2024-03-05 15:47:50 +0530

    Add semicolon

--- main/hello.cpp
+++ main/hello.cpp
@@ -8,6 +8,6 @@ int main(void) {
   cout << "I have successfully built and run\n";
   cout << "My name is Smaran!\n";
   cout << "My SRN is PES2UG21CS459 :)\n";
-  cout << "Github Webhook activated"
+  cout << "Github Webhook activated";
   return 0;
 }
